b2ac61db1091028a0e8106dc045de08c1b15b1d3
Sanket27 <sanketainchwar27@gmail.com>
2022-04-01 18:18:07 +0530

    added about route

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 app.get('/', (req,res)=>{
     res.send('Hello, welcome to Jenkins World!');
 })
+app.get('/about', (req,res)=>{
+    res.send('Hey, this is me, Sanket! Learning jenkins');
+})
+app.get('/:name', (req,res)=>{
+    res.send(`Hello, ${req.params.name}`);
+})
 
 app.listen('50505', ()=>{
     console.log('server up on port http://localhost:50505');
